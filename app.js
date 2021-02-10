92ecc3c9ff9cfc27529cdc2436148dc1924fbd73
unknown <hello.jewelahmed@gmail.com>
2021-02-11 00:09:14 +0600

    love site ready

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById("love-btn").addEventListener("click",function(){
+    const mainSection = document.getElementById("mainSection");
+    mainSection.style.display="none";
+
+    const love = document.getElementById("loveSection");
+    love.style.display ="block";
+
+    
+})
